90059281515be2f9b603829c50a3be9b35c1670e
Dima Dibb <dima.dibb1@gmail.com>
2020-06-05 00:05:34 -0700

    state

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,52 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from './component/Login';
 import Signup from './component/Signup';
 
-function App() {
-  return (
-    <div>
-      <Header />
+class App extends component {
+  constructor() {
+    super();
+    const token = localStorage.getItem("jwtToken");
+    const decoded = token != null ? jwt_decode(token) : {};
+    this.state = {
+      user: decoded,
+      token: token
+    }
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
 
-      <Router>
-        <Route exact path="/"component={() => <Home user="dima"/>} />
-        <Route exact path="/login"component={() => <Login />} />
-        <Route exact path="/Signup"component={() => <Signup/>} />
+  componentDidMount() {
+    const newState = {...this.state};
+    newState.redirect = false;
+    this.setState(newState);
+  }
 
-      </Router>
-      <AppFooter />
-    </div>
-  );
-}
+  handleLogin(token) {
+    token = token.replace("Bearer ", "");
+    localStorage.setItem("jwtToken", token);
+    const decoded = jwt_decode(token);
+    this.setState({user: decoded, token: token, redirect: true});
+  }
+  
+  handleLogout() {
+    localStorage.removeItem("jwtToken");
+    this.setState({user: {}, token: null, redirect: true});
+  };
 
-export default App;
+  render() {
+    return (
+      <Container>
+        <Header />
+
+        <Router>
+          <Route exact path="/" component={() => <Home user="dima" />} />
+          <Route exact path="/login" component={() => <Login />} />
+          <Route exact path="/Signup" component={() => <Signup />} />
+
+        </Router>
+        <AppFooter />
+      </Container>
+    );
+  }
+  }
+
+  export default App;
